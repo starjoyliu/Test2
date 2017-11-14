628aaebf805a5294c31287b6333ddce99e9de613
Star Liu <starjoyliu@virtualjoy.co>
2017-11-14 11:59:27 +0800

    1. add CustomDialog

--- variable/src/main/java/com/variable/Custom/CustomDialog.java
+++ variable/src/main/java/com/variable/Custom/CustomDialog.java
@@ -8,13 +8,11 @@ import android.support.annotation.Nullable;
 import com.variable.R;
 
 /**
+ * Custom full width dialog
  * Created by star on 2017/11/14.
  */
-
 public class CustomDialog extends Dialog{
     public CustomDialog(@NonNull Context context) {
         super(context, R.style.MATCH_DIALOG);
     }
-
-
 }

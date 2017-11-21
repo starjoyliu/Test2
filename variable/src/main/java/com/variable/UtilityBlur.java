6c9faf7753fdade822d67b022a9e30fcd323743e
Star Liu <star_liu@virtualjoy.co>
2017-11-21 18:01:37 +0800

    1. add UtilityBlur

---
+++ variable/src/main/java/com/variable/UtilityBlur.java
@@ -0,0 +1,87 @@
+package com.variable;
+
+import android.app.Activity;
+import android.graphics.Bitmap;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import jp.wasabeef.blurry.Blurry;
+
+/**
+ * Created by starliu on 2017/11/21.
+ */
+
+public class UtilityBlur {
+    private final int DEFAULT_SAMPLING = 2;
+
+    private volatile static UtilityBlur u;
+    public UtilityBlur(){}
+
+    public static UtilityBlur getNewInstance() {
+        if(u == null) {
+            synchronized (UtilityBlur.class) {
+                if(u == null) {
+                    u = new UtilityBlur();
+                }
+            }
+        }
+        return u;
+    }
+
+    /**
+     * blur view
+     * @param activity
+     * @param viewRoot
+     * @param view
+     * @param radius
+     */
+    public void blur(Activity activity, ViewGroup viewRoot, ImageView view, int radius){
+        blur(activity, viewRoot, view, radius, DEFAULT_SAMPLING);
+
+    }
+
+    /**
+     * blur view
+     * @param activity
+     * @param viewRoot
+     * @param view
+     * @param radius
+     * @param sampling
+     */
+    public void blur(Activity activity, ViewGroup viewRoot, ImageView view, int radius, int sampling){
+        // parent must be ViewGroup
+        Blurry.with(activity).radius(radius).sampling(sampling).onto(viewRoot);
+        // from View
+        Blurry.with(activity).capture(view).into(view);
+
+    }
+
+    /**
+     * blur bitmap
+     * @param activity
+     * @param viewRoot
+     * @param view
+     * @param bitmap
+     * @param radius
+     */
+    public void blurBitmap(Activity activity, ViewGroup viewRoot, ImageView view, Bitmap bitmap, int radius){
+        blurBitmap(activity, viewRoot, view, bitmap, radius, DEFAULT_SAMPLING);
+    }
+
+    /**
+     * blur bitmap
+     * @param activity
+     * @param viewRoot
+     * @param view
+     * @param bitmap
+     * @param radius
+     * @param sampling
+     */
+    public void blurBitmap(Activity activity, ViewGroup viewRoot, ImageView view, Bitmap bitmap, int radius, int sampling){
+        // parent must be ViewGroup
+        Blurry.with(activity).radius(radius).sampling(sampling).onto(viewRoot);
+        // from View
+        Blurry.with(activity).from(bitmap).into(view);
+    }
+}
